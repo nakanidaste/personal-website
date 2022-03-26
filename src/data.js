7f563a8f809f756bc036d1cd663aaeab9994eceb
Lullaby16 <samuelys945@gmail.com>
2022-03-26 12:22:11 +0700

    update productList

--- src/data.js
+++ src/data.js
@@ -1,19 +1,17 @@
-import Crypto from "./img/cryptoss.png";
-
 export const products = [
   {
     id: 1,
-    img: "https://www.pexels.com/photo/green-and-brown-coconut-trees-under-clear-blue-sky-2486168/",
+    img: require("./img/cryptoss.png").default,
     link: "https://karyakarsa.com/lullaby/cryptonews-app-114722",
   },
   {
     id: 2,
-    img: "https://www.pexels.com/photo/mountain-at-night-under-a-starry-sky-1624496/",
-    link: "http://lama.dev",
+    img: require("./img/skss.png").default,
+    link: "https://karyakarsa.com/lullaby/susunkatla",
   },
   {
     id: 3,
-    img: "https://www.pexels.com/photo/landscape-photography-of-snowy-mountain-1366919/",
-    link: "http://lama.dev",
+    img: require("./img/laundryss.png").default,
+    link: "https://github.com/Lullaby16/laundry-app",
   },
 ];
